e285c21442a8d779b6f1167a98c8a24fd3726a5e
Yazan Safadi <safadiyazan@gmail.com>
2024-03-04 15:32:38 +0200

    updates

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>CesiumJS Project Docs</span>,
+  logo: <span>CesiumJS Demo Docs</span>,
   project: {
     link: 'https://github.com/Safadiyazan/cesiumjs-demo',
   },
